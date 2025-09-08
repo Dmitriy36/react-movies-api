6c397a14a7b973e2f0478c7e51505c5624ffee57
Ignatius_Reilly <dmitriy_xix@hotmail.com>
2025-09-07 21:21:38 -0400

    asdf

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
-// import { HashRouter } from "react-router-dom";
+import { Favorites } from "./pages/Favorites.jsx";
 import "./css/index.css";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(<Favorites />);
